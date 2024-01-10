336c7ab8e100e7a61b10938236340deadbdfbad1
Christinne <vanessa0christinne@gmail.com>
2024-01-11 00:17:07 +0100

    Initial commit

---
+++ code/Zelda Guide/Zelda Guide/Zelda_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zelda_GuideApp.swift
+//  Zelda Guide
+//
+//  Created by Christinne on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Zelda_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
